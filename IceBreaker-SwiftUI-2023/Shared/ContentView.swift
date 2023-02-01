60ae8e486b05436dc35d12e764e7d2b350cb0c8e
Spencer Wondolowski <77039306+SpencerWondo@users.noreply.github.com>
2023-01-31 20:24:31 -0500

    UI Built for the App
    This is all of the UI built for the app!

--- IceBreaker-SwiftUI-2023/Shared/ContentView.swift
+++ IceBreaker-SwiftUI-2023/Shared/ContentView.swift
@@ -8,10 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var txtFirstName: String = ""
+    @State var txtLastName: String = ""
+    @State var txtPrefName: String = ""
+    
+    @State var txtQuestion: String = "Test"
+    @State var txtAnswer: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Placeholder")
+                .font(.system(size: 40))
+                .bold()
+            Text("By: S. Wondolowski")
+                .padding(.bottom)
+        
+            TextField("First Name:", text: $txtFirstName)
+            
+            TextField("Last Name:", text: $txtLastName)
+            
+            TextField("Pref Name:", text: $txtPrefName)
+            
+            Button(action: setQuestion) {
+                Text("Get Question")
+                    .padding(5)
+            }
+            
+            Text(txtQuestion)
+            
+            TextField("Answer: ", text: $txtAnswer)
+            
+            Button(action: sendAnswerToFirebase) {
+                Text("Submit")
+                    .padding(5)
+            }
+            
+        }
+        .multilineTextAlignment(.center)
+        .disableAutocorrection(true)
+        .onAppear(){
+            //Executes this code
+            //Fetch Questions form database
+            
+            getQuestionsFromFirebase()
+        }
+        
+        
     }
+    
+    
+    func getQuestionsFromFirebase(){
+        
+    }
+    
+    func setQuestion(){
+        txtQuestion = txtFirstName
+    }
+    
+    func sendAnswerToFirebase(){
+        txtQuestion = ""
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
