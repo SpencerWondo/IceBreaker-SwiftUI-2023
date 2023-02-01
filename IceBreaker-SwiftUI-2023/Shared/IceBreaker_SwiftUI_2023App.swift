3b14efa9e101aee0577588b30220eea7e3e9ff41
Spencer Wondolowski <77039306+SpencerWondo@users.noreply.github.com>
2023-01-31 20:59:04 -0500

    Added Firebase to the App

--- IceBreaker-SwiftUI-2023/Shared/IceBreaker_SwiftUI_2023App.swift
+++ IceBreaker-SwiftUI-2023/Shared/IceBreaker_SwiftUI_2023App.swift
@@ -5,13 +5,31 @@
 //  Created by forensics on 1/31/23.
 //
 
+
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
-struct IceBreaker_SwiftUI_2023App: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+struct YourApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
