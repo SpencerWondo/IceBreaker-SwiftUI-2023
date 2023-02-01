cbff016a4fb39f748184d0ebe41eff8135843037
Spencer Wondolowski <77039306+SpencerWondo@users.noreply.github.com>
2023-01-31 19:35:21 -0500

    Initial commit

---
+++ IceBreaker-SwiftUI-2023/Shared/IceBreaker_SwiftUI_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  IceBreaker_SwiftUI_2023App.swift
+//  Shared
+//
+//  Created by forensics on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct IceBreaker_SwiftUI_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
